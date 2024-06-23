f8377147ba41654acac97779fbbea28ee4273eae
Shreekant <shree.kant.74.sk@gmail.com>
2024-06-23 14:21:25 +0530

    custom hook added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import ".//style.css";
+import useFetch from "./useFetch";
 
-function App() {
+export default function App() {
+  return <Photos />;
+}
+
+function Photos() {
+  const { data, loading, error } = useFetch(
+    "https://jsonplaceholder.typicode.com/photos",
+    0,
+    2500
+  );
+  console.log(data);
+  if (loading) {
+    return (
+      <div className="app ">
+        <div className="loader">
+          <div className="cirle"></div>
+          <p className="load">Loading...</p>
+        </div>
+      </div>
+    );
+  }
+  if (error) {
+    return (
+      <div className="app ">
+        <div className="loader">Error:{error.message}</div>
+      </div>
+    );
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Photos</h1>
+      <div className="container">
+        {data.map((image) => (
+          <img
+            src={image.thumbnailUrl}
+            alt={image.title}
+            key={image.id}
+            className="photo"
+          />
+        ))}
+      </div>
     </div>
   );
 }
-
-export default App;
